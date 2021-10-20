f3c36cb9caf12f5ae378258d1f8dd21e879b2cd1
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-20 16:38:34 -0300

    Create authenticate in  github API

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+interface IAccessTokenResponse {
+    access_token: string
+}
+
+class AuthenticateUserService {
+    async execute(code: string) {
+        const url = "https://github.com/login/oauth/acess_token";
+
+        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url,null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code
+            },
+            headers: {
+                Accept: "application/json"
+            }
+        })
+
+        const response = await axios.get("https://api.github.com/user", {
+            headers: {
+                authorization: ` Bearer ${accessTokenResponse.access_token}`
+            }
+        })
+
+        return response.data;
+    }
+}
+
+export { AuthenticateUserService }
