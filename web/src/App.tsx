14b4c0a019a18ca2763a04616515a59e9a6cc0b5
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-21 22:13:30 -0300

    Configuração inicial para o front + divisão de pastas entre back e front

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import styles from './styles/App.module.scss'
+
+export function App() {
+
+  return (
+    <main className={styles.contentWrapper}>
+      <h2>Hello world</h2>
+    </main>
+  )
+}
+
+
