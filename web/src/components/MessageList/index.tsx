d38185f9d86b61bc93c5dd7d7c0544cea4bc3614
vitor barbosa <vitorbarbosa789@gmail.com>
2021-10-24 11:46:08 -0300

    hotload das mensagens com socket io

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -2,6 +2,7 @@ import styles from './styles.module.scss'
 import Logo from '../../assets/logo.svg'
 import { api } from '../../services/api'
 import { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 
 type Message = {
     id: string;
@@ -12,9 +13,31 @@ type Message = {
     }
 }
 
+const messageQueue: Message[] = []
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+    messageQueue.push(newMessage)
+})
+
 export function MessageList() {
 
     const [messages, setMessages] = useState<Message[]>([])
+
+    useEffect(() => {
+        setInterval(() => {
+            if(messageQueue.length > 0) {
+                setMessages(prevState =>[
+                    messageQueue[0],
+                    prevState[0],
+                    prevState[1]
+                ].filter(Boolean))
+
+                messageQueue.shift()
+            }
+        }, 3000)
+    })
 
     useEffect(() => {
         api.get<Message[]>('messages/last3').then(response => {
